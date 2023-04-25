891d0f9f058202621ca49d79e7e026e4ccfe8ece
Quuenore <atcheschev@yandex.ru>
2023-04-25 16:17:27 +0300

    gradle - correct dependencies

--- src/test/java/FirstAllureTest.java
+++ src/test/java/FirstAllureTest.java
@@ -1,9 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.currentTimeMillis;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class FirstAllureTest {
 
     @Test
     public void test() {
-
+        assertTrue(currentTimeMillis() > 0);
     }
 }
